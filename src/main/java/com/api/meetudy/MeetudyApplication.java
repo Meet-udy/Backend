0ce615dc6e6b13b940d365117eb3b7deae93a26e
hershyYJ <juj0478@naver.com>
2024-12-09 14:55:14 +0900

    [feature/#1] Initial commit

---
+++ src/main/java/com/api/meetudy/MeetudyApplication.java
@@ -0,0 +1,13 @@
+package com.api.meetudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/meetudy/MeetudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.meetudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
