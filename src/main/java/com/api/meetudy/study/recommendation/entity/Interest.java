876f3cc2a0e61851e8dc71f1033d2e3b0d6c462a
hershyYJ <juj0478@naver.com>
2024-12-23 17:17:08 +0900

    [feature/#8] Create Interest and MemberInterest entities to store user interests

---
+++ src/main/java/com/api/meetudy/study/recommendation/entity/Interest.java
@@ -0,0 +1,29 @@
+package com.api.meetudy.study.recommendation.entity;
+
+import com.api.meetudy.study.group.enums.StudyCategory;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Builder
+@Entity
+@ToString
+@Table(name  = "interest")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Interest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StudyCategory studyCategory;
+
+    @OneToMany(mappedBy = "interest", cascade = CascadeType.ALL)
+    private List<MemberInterest> memberInterests;
+
+}
