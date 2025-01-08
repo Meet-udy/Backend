33570ea7588e1fede5f04d634c686c1b4eb2afca
hershyYJ <juj0478@naver.com>
2025-01-08 17:14:38 +0900

    [remove/#14] Remove findByLeader method

--- src/main/java/com/api/meetudy/study/group/repository/GroupRepository.java
+++ src/main/java/com/api/meetudy/study/group/repository/GroupRepository.java
@@ -1,16 +1,11 @@
 package com.api.meetudy.study.group.repository;
 
-import com.api.meetudy.member.entity.Member;
 import com.api.meetudy.study.group.entity.StudyGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface GroupRepository extends JpaRepository<StudyGroup, Long>, JpaSpecificationExecutor<StudyGroup> {
-
-    List<StudyGroup> findByLeader(Member member);
 
 }
