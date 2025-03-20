37f5528605b54948433cedfd41888b24f84231fe
hershyYJ <juj0478@naver.com>
2025-03-20 16:22:44 +0900

    [feature/#20] Add @NotBlank annotation

--- src/main/java/com/api/meetudy/member/dto/UsernameDuplicationDto.java
+++ src/main/java/com/api/meetudy/member/dto/UsernameDuplicationDto.java
@@ -1,10 +1,12 @@
 package com.api.meetudy.member.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UsernameDuplicationDto {
 
+    @NotBlank
     private String username;
 
 }
